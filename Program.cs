29e3ec23ceb14c59536cd6409d08f6c53499510e
ozkut <alpozbaykut@gmail.com>
2023-04-12 18:09:36 +0300

    Nevermind still needed a few more fixes

--- Program.cs
+++ Program.cs
@@ -65,40 +65,37 @@ namespace TimedRickroll
             timeEntered = true;
 
             Timer DisplayCountdown_Timer = new(1000) { AutoReset = true };
-            DisplayCountdown_Timer.Elapsed += (source, e) => DisplayCountdown(enteredTime);
+            DisplayCountdown_Timer.Elapsed += (source, e) => 
+            {
+                Console.SetCursorPosition(0,3);
+                Console.Write($"Remaining time: {enteredTime - DateTime.Now.TimeOfDay + TimeSpan.FromSeconds(1):hh\\:mm\\:ss}");
+            };
             DisplayCountdown_Timer.Start();
 
             System.Threading.Thread.Sleep(20);
             DisplayTime_Timer.Start();
 
-            Timer timer = new((enteredTime - DateTime.Now.TimeOfDay).TotalMilliseconds) { AutoReset = false };
-            timer.Elapsed += (source, e) => TimerElapsed(link);
-            timer.Start();
+            Timer timer = new((enteredTime - DateTime.Now.TimeOfDay).TotalMilliseconds) { AutoReset = false, Enabled = true };
+            timer.Elapsed += (source, e) => 
+            {
+                Process process = new()
+                {
+                    StartInfo = new()
+                    {
+                        UseShellExecute = true,
+                        FileName = link
+                    }
+                };
+                process.Start();
+                DisplayTime_Timer.Stop();
+                DisplayCountdown_Timer.Stop();
+                Console.Clear();
+                Console.WriteLine("Press any key to exit...");
+            };
 
             Console.ReadKey();
-            DisplayTime_Timer.Stop();
-            DisplayCountdown_Timer.Stop();
             timer.Stop();
             Environment.Exit(Environment.ExitCode);
-        }
-
-        private static void TimerElapsed(string link)
-        {
-            Process process = new()
-            {
-                StartInfo = new()
-                {
-                    UseShellExecute = true,
-                    FileName = link
-                }
-            };
-            process.Start();
-        }
-
-        private static void DisplayCountdown(TimeSpan enteredTime)
-        {
-            Console.SetCursorPosition(0,3);
-            Console.Write($"Remaining time: {enteredTime - DateTime.Now.TimeOfDay + TimeSpan.FromSeconds(1):hh\\:mm\\:ss}");
         }
 
         private static void DisplayTime()
